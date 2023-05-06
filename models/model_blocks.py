7cb90079f5662bea0889f5eeed5e42bc106ade82
xuehongyang <xuehongyang@yiwise.com>
2023-05-06 18:51:40 +0800

    [feat] 修改Upsampling模块的结构

--- models/model_blocks.py
+++ models/model_blocks.py
@@ -111,9 +111,9 @@ class UpSamplingBlock(nn.Module):
         self,
     ):
         super(UpSamplingBlock, self).__init__()
-        self.net = nn.Sequential(ResBlock(256, 256, up_sample=True), ResBlock(256, 256, up_sample=True))
-        self.i_r_net = nn.Sequential(nn.LeakyReLU(0.2, inplace=True), nn.Conv2d(256, 3, 3, 1, 1))
-        self.m_r_net = nn.Sequential(nn.Conv2d(256, 1, 3, 1, 1), nn.Sigmoid())
+        self.net = nn.Sequential(ResBlock(256, 64, up_sample=True), ResBlock(64, 16, up_sample=True), ResBlock(16, 16))
+        self.i_r_net = nn.Sequential(nn.LeakyReLU(0.2, inplace=True), nn.Conv2d(16, 3, 3, 1, 1))
+        self.m_r_net = nn.Sequential(nn.Conv2d(16, 1, 3, 1, 1), nn.Sigmoid())
 
     def forward(self, x):
         x = self.net(x)
